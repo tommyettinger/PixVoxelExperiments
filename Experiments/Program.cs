2590ff78f9aa90a735eabd542a7ce5620dbc1b06
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-12-15 02:54:00 -0800

    Attempting to render voxels. Not yet.

---
+++ Experiments/Program.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+
+namespace Experiments
+{
+    class Experiment : GameWindow
+    {
+        /// <summary>Creates a 800x600 window with the specified title.</summary>
+        public Experiment()
+            : base(800, 600)
+        {
+            this.VSync = VSyncMode.Off;
+        }
+
+        #region --- Fields ---
+
+        const string vertex_shader =
+@"void main()
+{
+    gl_FrontColor = gl_Color;
+    gl_Position = ftransform();
+}";
+        const string fragment_shader =
+  @"void main()
+{
+    gl_FragColor = gl_Color;
+}";
+
+        int vertex_shader_object, fragment_shader_object, shader_program;
+        int vertex_buffer_object, color_buffer_object, element_buffer_object;
+        int texture;
+        Shape shape = null;
+        #endregion
+
+        #region OnLoad
+
+        /// <summary>
+        /// This is the place to load resources that change little
+        /// during the lifetime of the GameWindow. In this case, we
+        /// check for GLSL support, and load the shaders.
+        /// </summary>
+        /// <param name="e">Not used.</param>
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            
+            // Check for necessary capabilities:
+            Version version = new Version(GL.GetString(StringName.Version).Substring(0, 3));
+            Version target = new Version(2, 0);
+            if (version < target)
+            {
+                throw new NotSupportedException(String.Format(
+                    "OpenGL {0} is required (you only have {1}).", target, version));
+            }
+            Bitmap bitmap = new Bitmap("voxels.png");
+            GL.ClearColor(Color.DarkRed);
+            GL.Enable(EnableCap.DepthTest);
+
+            GL.Enable(EnableCap.Texture2D);
+
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            GL.GenTextures(1, out texture);
+            GL.BindTexture(TextureTarget.Texture2D, texture);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Nearest);
+
+            BitmapData data = bitmap.LockBits(new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            shape = new Voxels("zombie.bvx", 2);
+
+            CreateVBO();
+
+                CreateShaders(vertex_shader, fragment_shader,
+                    out vertex_shader_object, out fragment_shader_object,
+                    out shader_program);
+        }
+
+        #endregion
+
+        #region CreateShaders
+
+        void CreateShaders(string vs, string fs,
+            out int vertexObject, out int fragmentObject, 
+            out int program)
+        {
+            int status_code;
+            string info;
+
+            vertexObject = GL.CreateShader(ShaderType.VertexShader);
+            fragmentObject = GL.CreateShader(ShaderType.FragmentShader);
+
+            // Compile vertex shader
+            GL.ShaderSource(vertexObject, vs);
+            GL.CompileShader(vertexObject);
+            GL.GetShaderInfoLog(vertexObject, out info);
+            GL.GetShader(vertexObject, ShaderParameter.CompileStatus, out status_code);
+
+            if (status_code != 1)
+                throw new ApplicationException(info);
+
+            // Compile vertex shader
+            GL.ShaderSource(fragmentObject, fs);
+            GL.CompileShader(fragmentObject);
+            GL.GetShaderInfoLog(fragmentObject, out info);
+            GL.GetShader(fragmentObject, ShaderParameter.CompileStatus, out status_code);
+            
+            if (status_code != 1)
+                throw new ApplicationException(info);
+
+            program = GL.CreateProgram();
+            GL.AttachShader(program, fragmentObject);
+            GL.AttachShader(program, vertexObject);
+
+            GL.LinkProgram(program);
+            GL.UseProgram(program);
+        }
+
+        #endregion
+
+        #region private void CreateVBO()
+
+        void CreateVBO()
+        {
+            int size;
+
+            GL.GenBuffers(1, out vertex_buffer_object);
+            GL.GenBuffers(1, out color_buffer_object);
+            GL.GenBuffers(1, out element_buffer_object);
+
+            // Upload the vertex buffer.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertex_buffer_object);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(shape.Vertices.Length * 3 * sizeof(float)), shape.Vertices,
+                BufferUsageHint.StaticDraw);
+            GL.GetBufferParameter(BufferTarget.ArrayBuffer, BufferParameterName.BufferSize, out size);
+            if (size != shape.Vertices.Length * 3 * sizeof(Single))
+                throw new ApplicationException(String.Format(
+                    "Problem uploading vertex buffer to VBO (vertices). Tried to upload {0} bytes, uploaded {1}.",
+                    shape.Vertices.Length * 3 * sizeof(Single), size));
+
+            // Upload the color buffer.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, color_buffer_object);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(shape.Colors.Length * sizeof(int)), shape.Colors,
+                BufferUsageHint.StaticDraw);
+            GL.GetBufferParameter(BufferTarget.ArrayBuffer, BufferParameterName.BufferSize, out size);
+            if (size != shape.Colors.Length * sizeof(int))
+                throw new ApplicationException(String.Format(
+                    "Problem uploading vertex buffer to VBO (colors). Tried to upload {0} bytes, uploaded {1}.",
+                    shape.Colors.Length * sizeof(int), size));
+            
+            // Upload the index buffer (elements inside the vertex buffer, not color indices as per the IndexPointer function!)
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, element_buffer_object);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(shape.Indices.Length * sizeof(Int32)), shape.Indices,
+                BufferUsageHint.StaticDraw);
+            GL.GetBufferParameter(BufferTarget.ElementArrayBuffer, BufferParameterName.BufferSize, out size);
+            if (size != shape.Indices.Length * sizeof(int))
+                throw new ApplicationException(String.Format(
+                    "Problem uploading vertex buffer to VBO (offsets). Tried to upload {0} bytes, uploaded {1}.",
+                    shape.Indices.Length * sizeof(int), size));
+        }
+
+        #endregion
+
+        #region OnUnload
+
+        protected override void OnUnload(EventArgs e)
+        {
+            if (shader_program != 0)
+                GL.DeleteProgram(shader_program);
+            if (fragment_shader_object != 0)
+                GL.DeleteShader(fragment_shader_object);
+            if (vertex_shader_object != 0)
+                GL.DeleteShader(vertex_shader_object);
+            if (vertex_buffer_object != 0)
+                GL.DeleteBuffers(1, ref vertex_buffer_object);
+            if (element_buffer_object != 0)
+                GL.DeleteBuffers(1, ref element_buffer_object);
+        }
+
+        #endregion
+
+        #region OnResize
+
+        /// <summary>
+        /// Called when the user resizes the window.
+        /// </summary>
+        /// <param name="e">Contains the new width/height of the window.</param>
+        /// <remarks>
+        /// You want the OpenGL viewport to match the window. This is the place to do it!
+        /// </remarks>
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+
+            float aspect_ratio = Width / (float)Height;
+            GL.Ortho(0.0, Width, Height, 0.0, 1.0, -1.0);
+            GL.MatrixMode(MatrixMode.Projection);
+        }
+
+        #endregion
+
+        #region OnUpdateFrame
+
+        /// <summary>
+        /// Prepares the next frame for rendering.
+        /// </summary>
+        /// <remarks>
+        /// Place your control logic here. This is the place to respond to user input,
+        /// update object positions etc.
+        /// </remarks>
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            var keyboard = OpenTK.Input.Keyboard.GetState();
+            if (keyboard[OpenTK.Input.Key.Escape])
+                this.Exit();
+
+            if (keyboard[OpenTK.Input.Key.F11])
+                if (WindowState != WindowState.Fullscreen)
+                    WindowState = WindowState.Fullscreen;
+                else
+                    WindowState = WindowState.Normal;
+        }
+
+        #endregion
+
+        #region OnRenderFrame
+
+        /// <summary>
+        /// Place your rendering code here.
+        /// </summary>
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit |
+                     ClearBufferMask.DepthBufferBit);
+            GL.PushMatrix();
+            GL.Ortho(0.0, Width, Height, 0.0, 1.0, -1.0);
+
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.EnableClientState(ArrayCap.ColorArray);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vertex_buffer_object);
+            GL.VertexPointer(3, VertexPointerType.Float, 0, IntPtr.Zero);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, color_buffer_object);
+            GL.ColorPointer(4, ColorPointerType.UnsignedByte, 0, IntPtr.Zero);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, element_buffer_object);
+
+            GL.DrawElements(PrimitiveType.Triangles, shape.Indices.Length,
+                DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            //GL.DrawArrays(GL.Enums.BeginMode.POINTS, 0, shape.Vertices.Length);
+
+            GL.DisableClientState(ArrayCap.VertexArray);
+            GL.DisableClientState(ArrayCap.ColorArray);
+
+            GL.PopMatrix();
+            //int error = GL.GetError();
+            //if (error != 0)
+            //    Debug.Print(Glu.ErrorString(Glu.Enums.ErrorCode.INVALID_OPERATION));
+
+            SwapBuffers();
+        }
+
+        #endregion
+
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // The 'using' idiom guarantees proper resource cleanup.
+            // We request 20 UpdateFrame events per second, and unlimited
+            // RenderFrame events (as fast as the computer can handle).
+            using (Experiment example = new Experiment())
+            {
+                example.Title = "PixVoxel Demo";
+                example.Run(20.0, 0.0);
+            }
+        }
+    }
+}
---
+++ Experiments/Voxels.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenTK;
+using System.IO;
+
+namespace Experiments
+{
+    public struct Voxel
+    {
+        public byte x;
+        public byte y;
+        public byte z;
+        public byte color;
+    }
+    public class Voxels : Shape
+    {
+        
+        private const int White = (int)((0xff << 24) | 0xffffff);
+
+        public Voxel[] readBVX(string filename)
+        {
+            int size = 40;
+            List<Voxel> voxels = new List<Voxel>(size * size * size);
+            if (File.Exists(filename))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(filename, FileMode.Open)))
+                {
+                    int total = (int)(reader.BaseStream.Length);
+                    size = (int)(Math.Round(Math.Pow(total, 1.0 / 3.0)));
+                    byte[] bins = reader.ReadBytes(total);
+                    for (byte z = 0; z < size; z++)
+                    {
+                        for (byte y = 0; y < size; y++)
+                        {
+                            for (byte x = 0; x < size; x++)
+                            {
+                                if (bins[z * size * size + y * size + x] != 255)
+                                    voxels.Add(new Voxel { x = x, y = y, z = z, color = (byte)(253 - 4 * bins[z * size * size + y * size + x]) });
+                            }
+                        }
+                    }
+                }
+            }
+            return voxels.OrderBy(v => v.x * 64 - v.y + v.z * 64 * 128 - ((v.color == 253 - 100) ? 64 * 128 * 64 : 0)).ToArray();
+        }
+        public Voxels(string filename, int palette)
+        {
+            Voxel[] voxels = readBVX(filename);
+            Vertices = new Vector3[ 4 * voxels.Length];
+            for(int c = 0; c < voxels.Length; c++)
+            {
+                int vx = (voxels[c].x + voxels[c].y) * 2;
+                int vy = voxels[c].x  - voxels[c].y - voxels[c].z * 3;
+                Vertices[c * 4 + 0] = new Vector3(vx - 1.0f, vy - 1.0f, 0f);
+                Vertices[c * 4 + 1] = new Vector3(vx + 1.0f, vy - 1.0f, 0f);
+                Vertices[c * 4 + 2] = new Vector3(vx + 1.0f, vy + 1.0f, 0f);
+                Vertices[c * 4 + 3] = new Vector3(vx - 1.0f, vx + 1.0f, 0f);
+            };
+            
+            Normals = new Vector3[ 4 * voxels.Length];
+            for (int c = 0; c < 4 * voxels.Length; c++)
+            {
+                Normals[c] = new Vector3(0f, 0f, 1f);
+            };
+
+
+            int i = 0;
+            Indices = new int[6 * voxels.Length];
+            for (int c = 0; c < voxels.Length; c += 4)
+            {
+                Indices[i++] = c;
+                Indices[i++] = c + 2;
+                Indices[i++] = c + 1;
+
+                Indices[i++] = c + 1;
+                Indices[i++] = c + 2;
+                Indices[i++] = c + 3;
+            }
+
+
+            Colors = new int[4 * voxels.Length];
+            Colors.Fill(White);
+            /*
+top is    "(1.0 / 1024.0) * (5 * row)" and bottom is "(1.0 / 1024.0) * (5 * row + 4)"
+sides are "(1.0 / 1024.0) * (4 * column)" and        "(1.0 / 1024.0) * (4 * column + 4)"
+             */
+            Texcoords = new Vector2[4 * voxels.Length];
+            for (int c = 0; c < voxels.Length; c++)
+            {
+                Texcoords[c * 4 + 0] = new Vector2(0.0009765625f * (4 * voxels[c].color), 0.0009765625f * (5 * palette));//-1.0f, -1.0f
+                Texcoords[c * 4 + 1] = new Vector2(0.0009765625f * (4 * voxels[c].color + 4), 0.0009765625f * (5 * palette));//1.0f, -1.0f
+                Texcoords[c * 4 + 2] = new Vector2(0.0009765625f * (4 * voxels[c].color + 4), 0.0009765625f * (5 * palette + 4));//1.0f, 1.0f
+                Texcoords[c * 4 + 3] = new Vector2(0.0009765625f * (4 * voxels[c].color), 0.0009765625f * (5 * palette + 4));//-1.0f, 1.0f
+            };
+        }
+    }
+}
