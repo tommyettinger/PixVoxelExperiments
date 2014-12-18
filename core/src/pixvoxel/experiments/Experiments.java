3591a3de201579c5ef91f248567560db896e8188
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-12-17 17:27:26 -0800

    20fps with a small window. Thanks, @japro !

--- core/src/pixvoxel/experiments/Experiments.java
+++ core/src/pixvoxel/experiments/Experiments.java
@@ -15,9 +15,11 @@ import java.util.Random;
 public class Experiments extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
-    short[][][] voxels, culled;
+    short[][][] voxels;
+    short[][] culled;
+    int culled_length = 0;
     short[][] zbuffer, minbuffer, outlinebuffer, maxbuffer;
-    int xsize = 48 * 20, ysize = 48 * 20, zsize = 52;
+    short xsize = 48 * 20, ysize = 48 * 20, zsize = 52;
     int width = 800;
     int height = 600;
     long total_rendered = 0;
@@ -32,12 +34,12 @@ public class Experiments extends ApplicationAdapter {
         if (file.exists()) {
             byte[] bins = file.readBytes();
             double total = bins.length;
-            int size = (int) (Math.round(Math.cbrt(total)));
+            short size = (short) (Math.round(Math.cbrt(total)));
             vs = new short[size][size][size];
             for (short z = 0; z < size; z++) {
                 for (short y = 0; y < size; y++) {
                     for (short x = 0; x < size; x++) {
-                        vs[x][y][z] = (short) ((row << 8) | (0xff & bins[z * size * size + y * size + x]));
+                        vs[x][y][z] = (short) (((((0xff & bins[z * size * size + y * size + x]) == 255) ? 0 : row) << 8) | (0xff & bins[z * size * size + y * size + x]));
                     }
                 }
             }
@@ -66,13 +68,12 @@ public class Experiments extends ApplicationAdapter {
 		img = new Texture(Gdx.files.internal("voxels.png"), Pixmap.Format.RGBA8888, false);
 
         voxels = new short[xsize][ysize][zsize];
-        culled = new short[xsize][ysize][zsize];
+        culled = new short[xsize*ysize*5][4];
 
-        for (int x = 0; x < xsize; x++) {
-            for (int y = 0; y < ysize; y++) {
-                for (int z = 0; z < zsize; z++) {
+        for (short x = 0; x < xsize; x++) {
+            for (short y = 0; y < ysize; y++) {
+                for (short z = 0; z < zsize; z++) {
                     voxels[x][y][z] = 255;
-                    culled[x][y][z] = 255;
                 }
             }
         }
@@ -80,32 +81,11 @@ public class Experiments extends ApplicationAdapter {
                 grass = readBVX("Terrain", 50), sand = readBVX("Terrain", 52), mud = readBVX("Terrain", 54);
         short[][][][] terrains = {grass, sand, mud}, units = {zombie, zombie, male, female};
         for (int x = 0; x < 20; x++) {
-            for (int y = 0; y < 20; y++) {
-                insertModel(terrains[r.nextInt(3)], x * 48, y * 48, 0);
+            for (int y = 0; y < 20; y++) {                insertModel(terrains[r.nextInt(3)], x * 48, y * 48, 0);
                 if(r.nextInt(6) == 0)
-                    insertModel(units[r.nextInt(4)], x * 48, y * 48, 12);
+                    insertModel(units[r.nextInt(4)], x * 48 + 4, y * 48 + 4, 12);
             }
         }
-        for (int sx = 0; sx < width * 2; sx += 2) {
-            for (int sy = 0; sy < height * 2; sy++) {
-                for (int vz = zsize - 1; vz >= 0; vz--) {
-                    int vx = (2*height - 2*sy + sx + 2*vz * 3)/4;
-                    int vy = (2*sy + sx - 2*height - 2*vz * 3)/4;
-                    /*
-                    vy = (2*sy + sx - 2*height - 2*vz * 3)/4
-                    vx = (2*height - 2*sy + sx + 2*vz * 3)/4
-                    THANK YOU Jakob Progsch!
-                     */
-
-                    if(vx > 0 && vy > 0 && vx < xsize && vy < ysize && voxels[vx][vy][vz] != 255)
-                    {
-                        culled[vx][vy][vz] = voxels[vx][vy][vz];
-                        break;
-                    }
-                }
-            }
-        }
-
         minbuffer = new short[width * 2][height * 2];
         maxbuffer = new short[width * 2][height * 2];
         for(int i = 0; i < width * 2; i++)
@@ -116,11 +96,58 @@ public class Experiments extends ApplicationAdapter {
         zbuffer = minbuffer.clone();
         outlinebuffer = maxbuffer.clone();
 
+        boolean visible = false;
+        for (short z = (short)(zsize - 1); z >= 0 ; z--) {
+            for (short x = (short) (xsize - 1); x >= 0; x--) {
+                for (short y = 0; y < ysize; y++) {
+                    currentX = (x + y) * 2;
+                    currentY = height + y - x + z * 3;
+                    if ((voxels[x][y][z] & 0xff) == 255 || currentX < 0 || currentY < 0 || currentX >= width * 2 - 4 || currentY >= height * 2 - 4)
+                        continue;
+                    for (int ix = 0; ix < 4; ix++) {
+                        for (int iy = 0; iy < 4; iy++) {
+                            if (zbuffer[ix + currentX][iy + currentY] == -9999) {
+                                zbuffer[ix + currentX][iy + currentY] = (short) (z + x - y);
+                                visible = true;
+                            }
+                        }
+                    }
+                    if (visible) {
+                        culled[culled_length++] = new short[]{x, y, z, voxels[x][y][z]};
+                        visible = false;
+                    }
+                }
+            }
+        }
+        /*
+        for (int sx = 0; sx < width * 2; sx += 2) {
+            for (int sy = 0; sy < height * 2; sy++) {
+                for (int vz = zsize - 1; vz >= 0; vz--) {
+                    int vx = (2*height - 2*sy + sx + 2*vz * 3)/4;
+                    int vy = (2*sy + sx - 2*height - 2*vz * 3)/4;
+                    /*
+                    vy = (2*sy + sx - 2*height - 2*vz * 3)/4
+                    vx = (2*height - 2*sy + sx + 2*vz * 3)/4
+                    THANK YOU Jakob Progsch!
+                    /
+
+                    if(vx >= 0 && vy >= 0 && vx < xsize && vy < ysize && voxels[vx][vy][vz] != 255)
+                    {
+                        culled[counter++] = voxels[vx][vy][vz];
+                        break;
+                    }
+                }
+            }
+        }
+        */
+        //zbuffer = minbuffer.clone();
+
         cam = new OrthographicCamera(width * 2, height * 2);
         cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
         cam.update();
 	}
     private int currentX = 0, currentY = 0;
+    private short[] current_voxel;
 	@Override
 	public void render () {
         zbuffer = minbuffer.clone();
@@ -132,14 +159,16 @@ public class Experiments extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.8f, 0.5f, 1.0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-        for (int z = 0; z < zsize; z++) {
+/*        for (int z = 0; z < zsize; z++) {
             for (int x = 0; x < xsize; x++) {
-                for (int y = ysize - 1; y >= 0; y--) {
-                    short color =culled[x][y][z];
-                    if((color & 0xff) != 255)
+                for (int y = ysize - 1; y >= 0; y--) {*/
+        for(int c = culled_length - 1; c >= 0; c--)
+        {
+                    current_voxel =culled[c];
+                    if((current_voxel[3] & 0xff) != 255)
                     {
-                        currentX = (x + y) * 2;
-                        currentY = height + y - x + z * 3;
+                        currentX = (current_voxel[0] + current_voxel[1]) * 2;
+                        currentY = height + current_voxel[1] - current_voxel[0] + current_voxel[2] * 3;
                         if(currentX < 0)
                             continue;
                         if(currentY < 0)
@@ -151,16 +180,16 @@ public class Experiments extends ApplicationAdapter {
 
                         for(int ix = 0; ix < 4; ix++) {
                             for (int iy = 0; iy < 4; iy++) {
-                                zbuffer[ix + currentX][iy + currentY] = (short)(z + x - y);
-                                outlinebuffer[ix + currentX][iy + currentY] = color;
+                                zbuffer[ix + currentX][iy + currentY] = (short)(current_voxel[2] + current_voxel[0] - current_voxel[1]);
+                                outlinebuffer[ix + currentX][iy + currentY] = current_voxel[3];
                             }
                         }
-                        batch.draw(img, currentX, currentY, 4 * (color & 0xff), 5 * (color >> 8), 4, 4);
+                        batch.draw(img, currentX, currentY, 4 * (current_voxel[3] & 0xff), 5 * (current_voxel[3] >> 8), 4, 4);
                         total_rendered++;
                     }
                 }
-            }
-        }
+
+
 
         for (int x = 2; x < width * 2 - 2; x += 2) {
             for (int y = 2; y < height * 2 - 2; y += 2) {
