dc014ecfd922ff3c3afbfe9a78789ef9924760d0
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-12-25 23:05:13 -0800

    Unlimited FPS test

--- desktop/src/pixvoxel/experiments/desktop/DesktopLauncher.java
+++ desktop/src/pixvoxel/experiments/desktop/DesktopLauncher.java
@@ -6,7 +6,16 @@ import pixvoxel.experiments.Experiments;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Experiments(), config);
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.width = 800;
+        cfg.height = 600;
+
+        cfg.foregroundFPS = 0;
+        cfg.backgroundFPS = 0;
+       // cfg.fullscreen = true;
+        // vSync
+        cfg.vSyncEnabled = false;
+
+		new LwjglApplication(new Experiments(), cfg);
 	}
 }
